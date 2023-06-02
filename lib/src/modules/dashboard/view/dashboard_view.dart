a3fff975109a75577676587b58ffabba760f2f69
thwasim <fasilfasi9160@gmail.com>
2023-06-02 21:40:49 +0530

    slide animation clear

--- lib/src/modules/dashboard/view/dashboard_view.dart
+++ lib/src/modules/dashboard/view/dashboard_view.dart
@@ -1,5 +1,8 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_reorderable_grid_view/entities/order_update_entity.dart';
+import 'package:flutter_reorderable_grid_view/widgets/widgets.dart';
+import 'package:freelance/src/modules/custom/slide_and_remove.dart';
 
 class DashBoardView extends StatefulWidget {
   const DashBoardView({super.key});
@@ -9,6 +12,25 @@ class DashBoardView extends StatefulWidget {
 }
 
 class _DashBoardViewState extends State<DashBoardView> {
+  int selectedIndex = 0;
+  int currentIndex = 0;
+
+  List<String> items = [
+    "PAGE 1",
+    "PAGE 2",
+    "PAGE 3",
+    "PAGE 4",
+    "PAGE 5",
+    "TOTAL PAGE",
+  ];
+  static const _startCounter = 30;
+  final lockedIndices = <int>[];
+
+  List<int> children = List.generate(_startCounter, (index) => index);
+
+  final _scrollController = ScrollController();
+  final _gridViewKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +44,15 @@ class _DashBoardViewState extends State<DashBoardView> {
                   toolbarHeight: 100,
                   backgroundColor: Theme.of(context).primaryColor,
                   actions: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.search,
-                        color: Colors.white,
-                        size: 30,
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 35,
+                        ),
                       ),
                     )
                   ],
@@ -35,6 +60,7 @@ class _DashBoardViewState extends State<DashBoardView> {
                     onPressed: () {},
                     icon: const Icon(
                       Icons.menu,
+                      size: 35,
                       color: Colors.white,
                     ),
                   ),
@@ -42,19 +68,7 @@ class _DashBoardViewState extends State<DashBoardView> {
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.only(left: 4, right: 8, top: 4),
-                    child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 6,
-                        childAspectRatio: 1.2,
-                      ),
-                      itemCount: 30,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          color: Theme.of(context).primaryColor,
-                        );
-                      },
-                    ),
+                    child: _getReorderableWidget(),
                   ),
                 ),
                 SizedBox(
@@ -64,7 +78,6 @@ class _DashBoardViewState extends State<DashBoardView> {
               ],
             ),
           ),
-          
           Flexible(
             flex: 2,
             child: Column(
@@ -72,41 +85,80 @@ class _DashBoardViewState extends State<DashBoardView> {
                 AppBar(
                   backgroundColor: Colors.white,
                   toolbarHeight: 100,
+                  actions: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.dashboard_sharp,
+                          size: 35,
+                        ),
+                      ),
+                    )
+                  ],
                   title: InkWell(
                     onTap: () {
                       log("SAVED ITEMS SHOWING");
                     },
                     child: const Padding(
-                      padding: EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(10.0),
                       child: Text(
                         "Saved items  [${1}]",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
+                        style: TextStyle(color: Colors.black, fontSize: 25),
                       ),
                     ),
                   ),
                 ),
                 SizedBox(
                   height: MediaQuery.of(context).size.height * 0.79,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListView.builder(
-                      // Important: Remove any padding from the ListView.
-                      padding: EdgeInsets.zero,
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+                  child: ListView.builder(
+                    // Important: Remove any padding from the ListView.
+                    padding: EdgeInsets.zero,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return SlideBillMenu(
+                        menuItems: [
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.08,
+                            decoration: const BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(15),
+                                topRight: Radius.circular(15),
+                              ),
+                            ),
+                            child: IconButton(
+                              color: Colors.white,
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {},
+                            ),
                           ),
-                          contentPadding:
-                              const EdgeInsets.symmetric(horizontal: 8),
-                          title: Text('Item $index'),
-                          onTap: () {},
-                        );
-                      },
-                    ),
+                        ],
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.08,
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            trailing: const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                "1200",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 8),
+                            title: Text(
+                              'Item $index',
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                            onTap: () {},
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
                 Padding(
@@ -122,9 +174,7 @@ class _DashBoardViewState extends State<DashBoardView> {
                       ),
                       customBotton(
                         "CHARGE",
-                        () {
-                          log("message");
-                        },
+                        () {},
                       ),
                     ],
                   ),
@@ -219,17 +269,50 @@ class _DashBoardViewState extends State<DashBoardView> {
     );
   }
 
-  int selectedIndex = 0;
-  int currentIndex = 0;
+  void _handleReorder(List<OrderUpdateEntity> onReorderList) {
+    for (final reorder in onReorderList) {
+      final child = children.removeAt(reorder.oldIndex);
+      children.insert(reorder.newIndex, child);
+    }
+    setState(() {});
+  }
 
-  List<String> items = [
-    "PAGE 1",
-    "PAGE 2",
-    "PAGE 3",
-    "PAGE 4",
-    "PAGE 5",
-    "TOTAL PAGE",
-  ];
+  Widget _getReorderableWidget() {
+    final generatedChildren = List<Widget>.generate(
+      children.length,
+      (index) => Container(
+        key: Key(children[index].toString()),
+        decoration: BoxDecoration(
+          color: lockedIndices.contains(index) ? Colors.black : Colors.white,
+        ),
+        child: Card(
+          color: Theme.of(context).primaryColor,
+          child: Center(
+            child: Text(
+              'test ${children[index]}',
+              style: const TextStyle(),
+            ),
+          ),
+        ),
+      ),
+    );
+    return ReorderableBuilder(
+      key: Key(_gridViewKey.toString()),
+      onReorder: _handleReorder,
+      lockedIndices: lockedIndices,
+      scrollController: _scrollController,
+      builder: (children) {
+        return GridView.extent(
+          key: _gridViewKey,
+          controller: _scrollController,
+          maxCrossAxisExtent: 280,
+          padding: EdgeInsets.zero,
+          children: children,
+        );
+      },
+      children: generatedChildren,
+    );
+  }
 
   selectecdIndexUpdate(int index) {
     setState(() {
