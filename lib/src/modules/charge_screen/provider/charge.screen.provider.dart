f10be80b0ed6ad7bd8e5e839cfabbac6593e65a6
salahu01 <cvsalahu12@gmail.com>
2023-08-26 12:25:08 +0530

    fixed bugs in charge provider

--- lib/src/modules/charge_screen/provider/charge.screen.provider.dart
+++ lib/src/modules/charge_screen/provider/charge.screen.provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freelance/src/modules/charge_screen/provider/upload.reciept.provider.dart';
 
-final uploadRecieptProvider = StateNotifierProvider<UploadRecieptNotifier, String>((ref) {
+final uploadRecieptProvider = StateNotifierProvider.autoDispose<UploadRecieptNotifier, String>((ref) {
   return UploadRecieptNotifier();
 });
--- lib/src/modules/charge_screen/provider/upload.reciept.provider.dart
+++ lib/src/modules/charge_screen/provider/upload.reciept.provider.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freelance/src/core/models/reciept.model.dart';
 import 'package:freelance/src/core/services/db/local.db.sevices.dart';
