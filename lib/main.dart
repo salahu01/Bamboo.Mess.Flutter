b2b91872d392cb0fa75256d4dedde2bc117c89e6
thwasim <fasilfasi9160@gmail.com>
2023-09-15 17:43:43 +0530

    same changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freelance/src/core/models/reciept.model.dart';
 import 'package:freelance/src/core/services/db/local.db.sevices.dart';
@@ -8,7 +7,10 @@ import 'package:freelance/src/core/services/db/remote.db.services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freelance/src/modules/bluetooth_connection/bloc/bluethooth_connection_cubit.dart';
 import 'package:freelance/src/modules/dashboard/view/dashboard_view.dart';
+import 'package:freelance/src/modules/printing/printing_ui.dart';
 import 'package:hive_flutter/adapters.dart';
+// import 'dart:convert';
+// import 'package:drago_usb_printer/drago_usb_printer.dart';
 
 void main() async {
   await MongoDataBase().connectDb();
@@ -43,8 +45,84 @@ class MyApp extends StatelessWidget {
           fontFamily: "montserratfamily",
         ),
         debugShowCheckedModeBanner: false,
-        home: const DashBoardView(),
+        // home: const DashBoardView(),
+        home: const MyHomePage(title: "print sample"),
       ),
     );
   }
 }
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   _MyAppState createState() =>  _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   List<Map<String, dynamic>> devices = [];
+//   DragoUsbPrinter dragoUsbPrinter = DragoUsbPrinter();
+
+//   @override
+//   initState() {
+//     super.initState();
+//     _getDevicelist();
+//   }
+
+//   _getDevicelist() async {
+//     List<Map<String, dynamic>> results = [];
+//     results = await DragoUsbPrinter.getUSBDeviceList();
+
+//     print(" length: ${results.length}");
+//     setState(() {
+//       devices = results;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('USB PRINTER'),
+//           actions: <Widget>[
+//            IconButton(
+//                 icon: Icon(Icons.refresh),
+//                 onPressed: () => _getDevicelist()),
+//           ],
+//         ),
+//         body: devices.length > 0
+//             ? ListView(
+//                 scrollDirection: Axis.vertical,
+//                 children: _buildList(devices),
+//               )
+//             : null,
+//       ),
+//     );
+//   }
+
+//   List<Widget> _buildList(List<Map<String, dynamic>> devices) {
+//     return devices
+//         .map((device) => ListTile(
+//               leading: Icon(Icons.usb),
+//               title: Text(
+//                   device['manufacturer'] + " " + device['productName']),
+//               subtitle:
+//                  Text(device['vendorId'] + " " + device['productId']),
+//               trailing: IconButton(
+//                   icon: Icon(Icons.print),
+//                   onPressed: () async {
+//                     int vendorId = int.parse(device['vendorId']);
+//                     int productId = int.parse(device['productId']);
+//                     bool? isConnected =
+//                         await dragoUsbPrinter.connect(vendorId, productId);
+//                     if (isConnected ?? false) {
+//                       var data = Uint8List.fromList(utf8
+//                           .encode(" Hello world Testing ESC POS printer..."));
+//                       await dragoUsbPrinter.write(data);
+//                       await dragoUsbPrinter.close();
+//                     }
+//                   }),
+//             ))
+//         .toList();
+//   }
+// }
